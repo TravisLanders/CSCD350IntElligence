38f04b9730515ececeab6dfc16c48db84f6ae1ee
TravisLanders <travislanders@eagles.ewu.edu>
2016-05-02 15:03:56 -0700

    Added files via upload
    Committing Item abstraction with it's two subclasses.  Potions are just for temporary buffs or healing, and PermanentBuffs is obviously for increasing your stats for the rest of the game.

---
+++ PermanentBuffs.java
@@ -0,0 +1,31 @@
+
+package Items;
+
+public class PermanentBuffs  extends A_Item
+{
+
+	
+		private String name;
+		private String affects;
+		
+	
+	
+	@Override
+	public String getName() 
+	{
+		return this.name;
+	}
+
+	@Override
+	public String getAffects() 
+	{
+
+		return this.affects;
+	}
+	
+	public void performAdjustment()
+	{
+		//permanently increases certain attribute.
+	}
+
+}
---
+++ Potions.java
@@ -0,0 +1,36 @@
+package Items;
+
+public class Potions  extends A_Item
+{
+
+	
+		private String name;
+		private String affects;
+		private int duration;
+	
+	
+	@Override
+	public String getName() 
+	{
+		return this.name;
+	}
+
+	@Override
+	public String getAffects() 
+	{
+
+		return this.affects;
+	}
+	
+	public int getDuration(){return this.duration;}
+	public void setDuration()
+	{
+		this.duration--;
+	}
+	
+	public void performAdjustment()
+	{
+		
+	}
+
+}
